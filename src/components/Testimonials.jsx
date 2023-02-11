c97658832199c5f447e4ea9accf126885a82f58e
LauriOjanen <78724537+LauriOjanen@users.noreply.github.com>
2023-02-11 18:11:37 +0200

    Create testimonial
    Testimonials section created

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+<div className='w-1/2 mx-auto bg-gray-300 p-20'>
+  <div className='container mx-auto flex items-center justify-between'>
+    <p className='text-4xl font-light italic'>This is inspiring quote, of a testimonial from a customer. <br></br> Maybe it's
+      just filling up space, or maybe people will actuallu read it. <br></br>Who knows? All i know is that it looks nice
+    </p>
+  </div>
+  <div>
+  <p className='text-right font-bold'>-Thor, God Of Thunder</p>
+  </div>
+</div>
+  )
+}
+
+export default Testimonials
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import './index.css'
 import Navbar from './components/Navbar'
 import Main from './components/Main'
 import Card from './components/Card'
+import Testimonials from './components/Testimonials'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
     <Main />
     <Card />
+    <Testimonials />
   </React.StrictMode>,
 )
